7ea253068a5110a1ab238282e5932f9de91d2291
unknown <dleitedias@gmail.com>
2024-05-31 23:58:22 -0300

    operators

--- Program.cs
+++ Program.cs
@@ -1,7 +1,62 @@
-﻿using bootcamp.NET.models;
+﻿using System.Diagnostics;
+using bootcamp.NET.models;
+
+/*
+2- OPERADORES CONDICIONAIS COM SWITCH CASE
+
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine(); 
+
+switch (letra){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+
+    default:
+        Console.WriteLine("Consoante");
+        break;
+}
+*/
+
+
+
+
+/*
+2- OPERADORES CONDICIONAIS COM IF ELSE
+
+int quantidadeEmEstoque = 0; 
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (quantidadeCompra == 0) {
+    Console.WriteLine("A quantidade não pode ser zero.");
+}
+else if (possivelVenda) {
+    Console.WriteLine("Venda realizada!");
+}
+else {
+    Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque."); 
+    }
+*/
+
+
+
+
+
+/* 
+1- SINTAXE E INDENTAÇÃO
 
 Pessoa pessoa1 = new Pessoa();
 
 pessoa1.Nome = "Deborah";
 pessoa1.Idade = "25";
 pessoa1.Apresentar();
+*/
--- obj/Debug/net8.0/dotnetBootcamp.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnetBootcamp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetBootcamp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fac916f913dfe576496ecbdee9d2e8ad05ebee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a752ef5b1e3fc41e68fa835bb7fd6b5e53d1b7f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetBootcamp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetBootcamp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
